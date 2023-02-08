ea4a6df8bb3ef7d5689fa5aead0ff81cbb5e556d
NesDmitrius <ndmitrius@gmail.com>
2023-02-09 00:16:06 +0200

    dev

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,18 @@ import java.io.File;
 public class Main {
 
     private static long sizeFilesInDirectory;
+    private static String[] sizeNames = {"b", "Kb", "Mb", "Gb"};
 
     public static void main(String[] args) {
 
         String folderPath = "H:\\Видео\\Films\\Game.of.Thrones.s02.LostFilm";
         File file = new File(folderPath);
         System.out.println(getFolderSize(file));
+
+        String size = getHumanReadableSize(sizeFilesInDirectory);
+        System.out.println("Размер папки/файла: " + size);
+
+        System.out.println(getSizeFromHumanReadable("235K") + " bytes");
 
     }
 
@@ -24,6 +30,30 @@ public class Main {
             }
         }
         return sizeFilesInDirectory;
+    }
+
+    public static String getHumanReadableSize(long size) {
+        if (size == 0) {
+            return "Не найдены папка/файл";
+        }
+        int power = (int) (Math.log(size) / Math.log(1024));
+        double value = size / Math.pow(1024, power);
+        double roundedValue = Math.round(value * 100) / 100;
+        String result = roundedValue + " " + sizeNames[power];
+        return result;
+    }
+
+    public static long getSizeFromHumanReadable (String size) {
+        String regex = "[^0-9]";
+        String sizeNumber = size.replaceAll(regex, "");
+        int sizeFolder = Integer.parseInt(sizeNumber);
+        long sizeFolderInBytes = 0L;
+        for (int i = 0; i < sizeNames.length; i++) {
+            if (size.concat("b").contains(sizeNames[i])) {
+                sizeFolderInBytes = (long) (sizeFolder * Math.pow(1024, i));
+            }
+        }
+        return sizeFolderInBytes;
     }
 
 
